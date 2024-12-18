22b13faf00e6f6dcd5cca403c9ec1fde174c57da
Bati <94386708+BatiBati@users.noreply.github.com>
2024-12-19 07:34:13 +0800

    Checked again Data-types

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ console.log("hello world")
 //Data types//
 
 //1. String=> text type => "text"
-//2. Number=> number type => 1 6 1000
+//2. Number=> number type => 1, 6, 1000
 //3. Boolean => true or false type => false/true
 //4. Object => { key:value }
 //5. Array => [index, index ]
